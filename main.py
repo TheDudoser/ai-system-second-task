e7c7ac8ae2729060a35f6d02ed8ff1c509e4800e
Ismael Arce <ismael.arcev@gmail.com>
2024-12-14 20:04:30 +1000

    :sparkles: cli added

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
+import json
 from src.api_client import get_without_download_from_repo, get_token_by_current_env_vars, download_from_repo
 from src.extract_element_utils import find_nested_element
 from src.sim import Sim
+import typer
 
-if __name__ == '__main__':
-    # TODO: cli with 2 params: "ontology" path and "new case" path
-    path = "Загрузки / Онтология архива протоколов технологических операций лазерной обработки"
+app = typer.Typer()
 
-    test_data = get_without_download_from_repo(path, get_token_by_current_env_vars())
-
-    # TEST
+@app.command()
+def run_comparison(path: str, new_case_path: str, save_result: bool = False):
+    base = get_without_download_from_repo(path, get_token_by_current_env_vars())
+    new_case = get_without_download_from_repo(new_case_path, get_token_by_current_env_vars())
     key1 = "name"
     value1 = "Для целей тестирования"
     key2 = "name"
     value2 = "Техническое задание на выполнение технологической операции"
 
-    # Search for the element containing both key-value pairs
-    result = find_nested_element(test_data, key1, value1, key2, value2)
+    to_1 = find_nested_element(base, key1, value1, key2, value2)
+    to_2 = find_nested_element(new_case, key1, value1, key2, value2)
 
-    print(Sim.compare(result, result))
+    result = Sim.compare(to_1, to_2)
+
+    if save_result:
+        with open("result.json", "w") as f:
+            json.dump(result, f)
+            print("result saved")
+            print(result)
+    else:
+        print(result)
+
+
+
+
+if __name__ == "__main__":
+    app()
+
+    # TODO: cli with 2 params: "ontology" path and "new case" path
+    # path = "Загрузки / Онтология архива протоколов технологических операций лазерной обработки"
+
+    # test_data = get_without_download_from_repo(path, get_token_by_current_env_vars())
+
+    # # TEST
+    # key1 = "name"
+    # value1 = "Для целей тестирования"
+    # key2 = "name"
+    # value2 = "Техническое задание на выполнение технологической операции"
+
+    # # Search for the element containing both key-value pairs
+    # result = find_nested_element(test_data, key1, value1, key2, value2)
+
+    # print(Sim.compare(result, result))
