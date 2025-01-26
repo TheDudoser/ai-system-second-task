ca7e416b3a0d3db3f1affd65aadaf76a7bacfde3
Dudoserovich <mr.egoryk@mail.ru>
2025-01-26 22:22:14 +1000

    Added print for filter d1/d2

--- src/services/method_filter_and_match.py
+++ src/services/method_filter_and_match.py
@@ -1,11 +1,15 @@
 def filter_by_method_and_get_matching_objects(similarity_table, operation_dict, is_euklid):
     # Функция для фильтрации объектов по значению %d1
     filtered_table1 = []
+    percentage = 60
     for item in similarity_table["Table1" if not is_euklid else "Table2"]:
-        if item["data"].get("%d1" if not is_euklid else "%d2", 0) >= 60:
+        if item["data"].get("%d1" if not is_euklid else "%d2", 0) >= percentage:
             filtered_table1.append(item)
         if len(filtered_table1) == 6:  # Ограничиваем результат первыми 6 объектами
             break
+
+    print("В результате фильтрации по " + ("Манхетену" if not is_euklid else "Евклиду") + f" с порогом похожести выше {percentage}% осталось {len(filtered_table1)} операций")
+    print()
 
     # Извлекаем имена (TO_name) из выбранных объектов
     selected_names = [item["TO_name"] for item in filtered_table1]
